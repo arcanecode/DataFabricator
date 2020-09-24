2b365c59ddfb93db63c855d490c9790171d4cf89
ArcaneCode <arcanecode@gmail.com>
2020-09-24 13:11:46 -0500

    Updated to use variables for testing values

--- Tests/ConvertFrom-CityStateCode.Tests.ps1
+++ Tests/ConvertFrom-CityStateCode.Tests.ps1
@@ -4,23 +4,28 @@ Describe "ConvertFrom-CityStateCode Tests" {
     $module = 'DataFabricator'
     $function = 'ConvertFrom-CityStateCode'
     $here = $PSScriptRoot
-
-  }
+    $cityStateCodeUS = 'ALAARONSBURGCDP'
+    $cityStateCodeUK = 'ALSTDAVIDS'
+    $expectedCityUS = 'Aaronsburg CDP'
+    $expectedCityUK = 'St Davids'
+    $expectedStateUS = 'AL'
+    $expectedStateUK = 'AL'
+  } 
 
   Context "<module> <function> Tests" {
-    It "<function> Default CityCode of ALAARONSBURGCDP" {
-      $(ConvertFrom-CityStateCode -CityStateCode "ALAARONSBURGCDP").State | Should -Be 'AL'
-      $(ConvertFrom-CityStateCode -CityStateCode "ALAARONSBURGCDP").City  | Should -Be 'Aaronsburg CDP'
+    It "<function> Default Country Code CityStateCode of <cityStateCodeUS> returns <expectedCityUS>, <expectedStateUS>" {
+      $(ConvertFrom-CityStateCode -CityStateCode $cityStateCodeUS).State | Should -Be $expectedStateUS
+      $(ConvertFrom-CityStateCode -CityStateCode $cityStateCodeUS).City  | Should -Be $expectedCityUS
     }
 
-    It "<function> US CityCode of ALAARONSBURGCDP" {
-      $(ConvertFrom-CityStateCode -CountryCode US -CityStateCode "ALAARONSBURGCDP").State | Should -Be 'AL'
-      $(ConvertFrom-CityStateCode -CountryCode US -CityStateCode "ALAARONSBURGCDP").City  | Should -Be 'Aaronsburg CDP'
+    It "<function> CountryCode of US CityStateCode of <cityStateCodeUS> returns <expectedCityUS>, <expectedStateUS>" {
+      $(ConvertFrom-CityStateCode -CountryCode 'US' -CityStateCode $cityStateCodeUS).State | Should -Be $expectedStateUS
+      $(ConvertFrom-CityStateCode -CountryCode 'US' -CityStateCode $cityStateCodeUS).City  | Should -Be $expectedCityUS
     }
 
-    It "<function> UK CityCode of ALSTDAVIDS" {
-      $(ConvertFrom-CityStateCode -CountryCode UK -CityStateCode "ALSTDAVIDS").State | Should -Be 'AL'
-      $(ConvertFrom-CityStateCode -CountryCode UK -CityStateCode "ALSTDAVIDS").City  | Should -Be 'St Davids'
+    It "<function> CountryCode of UK CityStateCode of <cityStateCodeUK> returns <expectedCityUK>, <expectedStateUK>" {
+      $(ConvertFrom-CityStateCode -CountryCode 'UK' -CityStateCode $cityStateCodeUK).State | Should -Be $expectedStateUK
+      $(ConvertFrom-CityStateCode -CountryCode 'UK' -CityStateCode $cityStateCodeUK).City  | Should -Be $expectedCityUK
     }
   }
 
